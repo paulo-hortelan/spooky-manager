57f229a02346413f3fe2434f90cf736a4835dd8c
paulo-hortelan <paulo.ribeiro@aonet.com.br>
2024-10-30 18:02:21 -0300

    imp

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,18 @@
 
 use Livewire\Volt\Volt;
 
-Volt::route('/', 'users.index');
+Volt::route('/', 'auth.login');
+
+// Route::get('/', Welcome::class)
+//     ->middleware(['auth', 'verified'])
+//     ->name('home');
+
+// // Authentication Routes
+
+// Route::get('login', Login::class)
+//     ->middleware('guest')
+//     ->name('login');
+
+// Route::get('register', Register::class)
+//     ->middleware('guest')
+//     ->name('register');
